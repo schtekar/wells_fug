3d4fd0ba7cbbb512e71ecfb4c074100a1812e40d
schtekar <schtekar@gmail.com>
2025-12-29 18:46:52 +0100

    Create rw_analysis_config.py

---
+++ scripts/config/rw_analysis_config.py
@@ -0,0 +1,8 @@
+REFERENCE_POSITION_OPTIONS = {
+    "12h": {"source": "bw_snapshots", "tag": "msg_12h"},
+    "1d": {"source": "bw_snapshots", "tag": "msg_1d"},
+    "2d": {"source": "bw_snapshots", "tag": "msg_2d"},
+    "5d": {"source": "kdh_filtered", "tag": "5d_last"},
+    "1w": {"source": "kdh_filtered", "tag": "1w_last"},
+    "1mo": {"source": "kdh_filtered", "tag": "1mo_last"},
+}
