f49ee38cf22f7dd165dce81b6b5a3a5061ebfc20
schtekar <schtekar@gmail.com>
2025-12-31 12:47:03 +0100

    Update icons.js

--- docs/js/icons.js
+++ docs/js/icons.js
@@ -31,18 +31,11 @@ export function wellColor(well) {
 export function rigIcon(color) {
   return L.divIcon({
     className: "",
-    html: `<svg width="27" height="31" viewBox="0 0 27 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <!-- Main rig body -->
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M16.7 14 15 1h-3l-1.7 13H5v4H1v3h4v9h5.4v-9h6.2v9H22v-9h4v-3h-4v-4h-5.3Z" fill="${color}"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="m15.8 0 1.8 13H23v4h4v5h-4v9h-7.4v-9h-4.2v9H4v-9H0v-5h4v-4h5.4L11 0h4.8Zm-5.6 14H5v4H1v3h4v9h5.4v-9h6.2v9H22v-9h4v-3h-4v-4h-5.3L15 1h-3l-1.7 13Z" fill="#191919"/>
-      <path d="M10.4 16v14H5V16h5.4ZM22 16v14h-5.4V16H22Z" fill="${color}"/>
-      <path d="M22 14v7H5v-7h17Z" fill="${color}"/> 
-      <path d="M1 18h25v3H1v-3Z" fill="${color}"/>
-      <path d="m15 1 2 15h-7l1.9-15h3Z" fill="${color}"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 18H4.5v-1H23v1Z" fill="#191919"/>
+    html: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 8.2 9.2" fill="none"> 
+  <path d="M0.1 5.1 0.1 7.1 h1 v2 h2 v-2 h2 v2 h2 v-2 h1 v-2 h-2 l-1 -5 h-2 l-1 5 h-2 Z" fill="${color}" stroke="#191919" stroke-width="0.2"/>
     </svg>`,
-    iconSize: [27, 31],
-    iconAnchor: [13.5, 31]
+    iconSize: [24, 24],
+    iconAnchor: [13.5, 24]
   });
 }
 
